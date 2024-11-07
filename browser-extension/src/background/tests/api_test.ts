2a1b9cf105db4b54c9301a15e590c8109debb344
gpg4b <gpg4b@proton.me>
2024-11-07 21:22:36 +0300

    core API tests: mnemonic tests added

--- browser-extension/src/background/tests/api_test.ts
+++ browser-extension/src/background/tests/api_test.ts
@@ -1,4 +1,6 @@
 import { describe, expect, test } from 'vitest'
+import wordlist from 'web-bip39/wordlists/english';
+
 
 import utxord from '~/libs/utxord.js'
 
@@ -36,4 +38,25 @@ describe('CoreAPI', async () => {
     console.log("Tx structure: ", txJson)
 
   })
+
+  describe('Mnemonics', async () => {
+
+    const mnemonicParser = new api.MnemonicParser(`["${wordlist.join('","')}"]`);
+    const mnemonicPhrase = 'vendor patrol opinion thumb clock teach chalk stomach bike body rent infant';
+    const mnemonicEntropyHex = 'f234266df0b2b7bd097eb116231ed939';
+
+    test('GenerateMnemonic', () => {
+      let phrase = null;
+      expect(() => {phrase = mnemonicParser.EncodeEntropy(mnemonicEntropyHex)}).not.toThrowError();
+      expect(phrase).toEqual(mnemonicPhrase);
+    })
+
+    test('ValidateMnemonic', () => {
+      let entropy = null;
+      expect(() => {entropy = mnemonicParser.DecodeEntropy(mnemonicPhrase)}).not.toThrowError();
+      expect(entropy).toEqual(mnemonicEntropyHex);
+    })
+
+  })
+
 })
