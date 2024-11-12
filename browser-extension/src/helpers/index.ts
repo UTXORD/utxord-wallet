fc32845fc75ca9b3881645736d81c5270d9aebef
DevOraTor18 <devorator18@gmail.com>
2024-11-12 06:49:47 +0300

    Add detectBrowser and disable panel for firefox

--- browser-extension/src/helpers/index.ts
+++ browser-extension/src/helpers/index.ts
@@ -147,3 +147,20 @@ export function validateBtcAddressInfo(address: string){
     return `Address is incorrect`
   }
 }
+
+export function detectBrowser() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    if (userAgent.indexOf('firefox') !== -1) {
+        return 'Firefox';
+    } else if (userAgent.indexOf('chrome') !== -1) {
+        return 'Chrome';
+    } else if (userAgent.indexOf('safari') !== -1) {
+        return 'Safari';
+    } else if (userAgent.indexOf('opera') !== -1 || userAgent.indexOf('opr') !== -1) {
+        return 'Opera';
+    } else if (userAgent.indexOf('msie') !== -1 || userAgent.indexOf('trident') !== -1) {
+        return 'Internet Explorer';
+    } else {
+        return 'Unknown';
+    }
+}
