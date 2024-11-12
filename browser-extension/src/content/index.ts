a5785cb8ae0cf634024b2151d97efeb989d7a7ee
denisoed <denisoed@gmail.com>
2024-11-12 16:30:10 +0600

    UT-2233 Added new message listener to content script

--- browser-extension/src/content/index.ts
+++ browser-extension/src/content/index.ts
@@ -1,9 +1,20 @@
-/* eslint-disable no-console */
-import { onMessage } from 'webext-bridge'
-
-console.info('[browser-ext-mv3-starter] Hello world from content script')
+import { onMessage, sendMessage } from 'webext-bridge'
 
 // communication example: send previous tab title from background page
 onMessage('tab-prev', ({ data }) => {
-  console.log(`[browser-ext-mv3-starter] Navigate from page "${data.title}"`)
+  console.log(`[browser-ext-mv3-starter] Navigate from page '${data.title}'`)
 })
+
+// Handle messages from the page
+window.addEventListener('message', async (event) => {
+  // Check that the message is coming from the same page (not from third-party scripts)
+  if (event.origin !== window.location.origin) return;
+
+  // Check the message is from the page
+  if (event.data && event.data.from === 'MESSAGE_FROM_WEB') {
+    // Send message to Background Script
+    sendMessage(event.data.type, event.data.payload, 'background');
+  } else {
+    console.warn(`Unallowed message from ${event.data.from}`);
+  }
+});
