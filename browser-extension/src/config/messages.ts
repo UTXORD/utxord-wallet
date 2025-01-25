cdfde9ad512a4d0a4629f83266adc3350611933e
gpg4b <gpg4b@proton.me>
2025-01-25 16:58:09 +0300

    fix: added missing config/messages.ts file

---
+++ browser-extension/src/config/messages.ts
@@ -0,0 +1,4 @@
+export const INSUFFICIENT_FUNDS = "Insufficient funds. Please add."
+export const COLLECTION_NOT_FOUND = "Collection is not found in balances."
+export const PLEASE_UPDATE_PLUGIN = 'Please update the plugin to latest version.'
+
